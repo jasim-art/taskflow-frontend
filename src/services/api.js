0e2572c0c224dad39ae8d1f5db5ba51c0a9d41ff
jasim-art <msjasim959@gmail.com>
2026-06-09 23:36:11 +0530

    Connect production backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL:
+    "https://taskflow-backend-fz9a.onrender.com/api"
 });
 
 export default api;
